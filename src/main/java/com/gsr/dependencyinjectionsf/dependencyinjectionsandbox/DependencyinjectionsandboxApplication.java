70ac89cb9214f40e7a56894c697648cda62481a6
Sasinda98 <sasindarupasighe@icloud.com>
2021-07-29 13:00:34 +0530

    initial commit.

---
+++ src/main/java/com/gsr/dependencyinjectionsf/dependencyinjectionsandbox/DependencyinjectionsandboxApplication.java
@@ -0,0 +1,13 @@
+package com.gsr.dependencyinjectionsf.dependencyinjectionsandbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DependencyinjectionsandboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DependencyinjectionsandboxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gsr/dependencyinjectionsf/dependencyinjectionsandbox/DependencyinjectionsandboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gsr.dependencyinjectionsf.dependencyinjectionsandbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DependencyinjectionsandboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
